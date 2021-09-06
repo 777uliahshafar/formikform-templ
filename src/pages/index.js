273bc3c9a9dd723eb95067fa0ed27a051bbc1a37
Muhammad Shafar <777uliahshafar@gmail.com>
2021-09-06 14:33:58 +0800

    done basicForm

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import SEO from '../components/seo'
 import Layout from '../components/layout'
 
-import { Form, Button, Container } from 'react-bootstrap'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 import { navigate } from 'gatsby'
 
 const IndexPage = () => {
@@ -73,7 +73,7 @@ const IndexPage = () => {
             />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPhone">
+          <Form.Group controlId="formPhone">
             <Form.Label>Nomor HP (Opsional)</Form.Label>
             <Form.Control
               type="text"
@@ -83,32 +83,151 @@ const IndexPage = () => {
               placeholder="08135519***"
             />
           </Form.Group>
-
           <fieldset>
-            <Form.Group controlId="formBasicGender">
-              <Form.Label>Jenis Kelamin:</Form.Label>
-              <Form.Check
-                type="radio"
-                label="laki-laki"
-                name="gender"
-                id="lakiLaki"
-                onChange={handleChange}
-                value={formState.gender}
-              />
+            <Form.Group as={Row} className="mb-3" controlId="formGender">
+              <Form.Label column sm={2}>
+                Jenis Kelamin:
+              </Form.Label>
 
-              <Form.Check
-                type="radio"
-                label="perempuan"
-                name="gender"
-                id="perempuan"
-                onChange={handleChange}
-                value={formState.gender}
-              />
+              <Col sm={10}>
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="laki-laki"
+                  name="gender"
+                  id="lakiLaki"
+                  onChange={handleChange}
+                  value={formState.gender}
+                />
+
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="perempuan"
+                  name="gender"
+                  id="perempuan"
+                  onChange={handleChange}
+                  value={formState.gender}
+                />
+              </Col>
             </Form.Group>
           </fieldset>
 
+          <fieldset>
+            <Form.Group as={Row} className="mb-3" controlId="formSuku">
+              <Form.Label column sm={2}>
+                Suku:
+              </Form.Label>
+              <Col sm="10">
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Bugis"
+                  name="suku"
+                  id="bugis"
+                  onChange={handleChange}
+                  value={formState.suku}
+                />
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Makassar"
+                  name="suku"
+                  id="makassar"
+                  onChange={handleChange}
+                  value={formState.suku}
+                />
+
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Toraja"
+                  name="suku"
+                  id="toraja"
+                  onChange={handleChange}
+                  value={formState.suku}
+                />
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Lainnya"
+                  name="suku"
+                  id="lainLain"
+                  onChange={handleChange}
+                  value={formState.suku}
+                />
+              </Col>
+            </Form.Group>
+          </fieldset>
+
+          <Form.Group controlId="formTahunLahir">
+            <Form.Label>Tahun lahir:</Form.Label>
+            <Form.Control
+              type="text"
+              name="tahunLahir"
+              onChange={handleChange}
+              value={formState.tahunLahir}
+            />
+          </Form.Group>
+
+          <fieldset>
+            <Form.Group as={Row} className="mb-3" controlId="formJob">
+              <Form.Label column sm={10}>
+                Pekerjaan:
+              </Form.Label>
+              <Col sm="10">
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Karyawan"
+                  name="job"
+                  id="karyawan"
+                  onChange={handleChange}
+                  value={formState.job}
+                />
+
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Wiraswasta"
+                  name="job"
+                  id="wiraswasta"
+                  onChange={handleChange}
+                  value={formState.job}
+                />
+
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Belum bekerja"
+                  name="job"
+                  id="belumBekerja"
+                  onChange={handleChange}
+                  value={formState.job}
+                />
+
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Pelajar"
+                  name="job"
+                  id="pelajar"
+                  onChange={handleChange}
+                  value={formState.job}
+                />
+              </Col>
+            </Form.Group>
+          </fieldset>
+
+          <p>
+            Jika uang, waktu dan halangan lainnya bukan pertimbangan, ruang mana
+            yang anda lebih cenderungi untuk dikunjungi? Silakan nilai dengan
+            skala poin 100 dimana ``1" artinya ``kurang dicenderungi" dan ``100"
+            artinya ``Sangat dicenderungi""
+          </p>
+
           <Button variant="primary" type="submit">
-            Kirim Kuesioner
+            Kirimkan
           </Button>
         </Form>
       </Container>
