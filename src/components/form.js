367e64e216e2dba505a8a5a0fbadffb128254828
Muhammad Shafar <777uliahshafar@gmail.com>
2021-09-15 07:38:47 +0800

    form state formik

--- src/components/form.js
+++ src/components/form.js
@@ -1,74 +1,39 @@
 import React, { useState } from 'react'
-import { Formik } from 'formik'
+import { useFormik, Formik } from 'formik'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Form, Button, Container } from 'react-bootstrap'
 
 const IndexPage = () => {
-  const [formState, setFormState] = useState({
-    nomorHp: ''
+  const formik = useFormik({
+    initialValues: {
+      nomorHp: ''
+    }
   })
 
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-      )
-      .join('&')
-  }
-
-  const handleChange = (e) => {
-    setFormState({
-      ...formState,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  const handleSubmit = (e) => {
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', ...this.state })
-    })
-      .then(() => navigate('/terima-kasih/'))
-      .catch((error) => alert(error))
-
-    e.preventDefault()
-  }
-
   return (
-    <Formik>
-      <Container>
-        <Form
-          onSubmit={handleSubmit}
-          name="formik v1"
-          method="post"
-          data-netlify="true"
-          action="/terima-kasih"
-          data-netlify-honeypot="bot-field"
-        >
-          <p hidden>
-            <label>
-              I love honeypot : <input name="bot-field" />
-            </label>
-          </p>
-          <input type="hidden" name="form-name" value="formik v1" />
-
-          <Form.Group controlId="formPhone">
-            <Form.Label>Nomor HP (Opsional)</Form.Label>
-            <Form.Control
-              type="text"
-              name="nomorHp"
-              onChange={handleChange}
-              value={formState.nomorHp}
-              placeholder="08135519***"
-            />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Kirimkan
-          </Button>
-        </Form>
-      </Container>
+    <Formik
+      initialValues={{
+        nomorHp: ''
+      }}
+    >
+      {(formik) => (
+        <Container>
+          {console.log('Form values', formik.values)}
+          <Form>
+            <Form.Group>
+              <Form.Label>Nomor HP (Opsional)</Form.Label>
+              <Form.Control
+                type="text"
+                id="nomorHp"
+                name="nomorHp"
+                onChange={formik.handleChange}
+                value={formik.values.nomorHp}
+                placeholder="08135519***"
+              />
+            </Form.Group>
+          </Form>
+        </Container>
+      )}
     </Formik>
   )
 }
