d23c4a92638629883b9cc7f6b1d2a68bd8c1b828
Muhammad Shafar <777uliahshafar@gmail.com>
2021-09-15 17:12:04 +0800

    refactoring component

--- src/components/form.js
+++ src/components/form.js
@@ -1,28 +1,14 @@
 import React from 'react'
-import { useFormik } from 'formik'
-import * as Yup from 'yup'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Formik, Field, ErrorMessage } from 'formik'
 import { Form, Button, Container } from 'react-bootstrap'
 import './form.css'
+import * as Yup from 'yup'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const initialValues = {
-  nomorHp: ''
-}
+const initialValues = { nomorHp: '' }
 
 const onSubmit = (values) => {
   console.log('Form data', values)
-}
-
-const validate = (values) => {
-  // values.nomorHp
-  // errors.nomorHp
-  // errors.nomorHp =  "isian ini dibutuhkan"
-
-  let errors = {}
-  if (!values.nomorHp) {
-    errors.nomorHp = 'Required'
-  }
-  return errors
 }
 
 const validationSchema = Yup.object().shape({
@@ -30,41 +16,34 @@ const validationSchema = Yup.object().shape({
 })
 
 const IndexPage = () => {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema
-    //    validate
-  })
-
   return (
     <Container>
-      {console.log('Visited field', formik.errors)}
-      <Form onSubmit={formik.handleSubmit}>
-        <Form.Group>
-          <Form.Label>Nomor HP (Opsional)</Form.Label>
-          <Form.Control
-            type="text"
-            id="nomorHp"
-            name="nomorHp"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.nomorHp}
-            placeholder="08135519***"
-            className={
-              formik.touched.nomorHp && formik.errors.nomorHp ? 'error' : null
-            }
-          />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(formik) => {
+          return (
+            <Form onSubmit={formik.handleSubmit}>
+              <Form.Group>
+                <Form.Label>Nomor HP (Opsional)</Form.Label>
+                <Field
+                  type="text"
+                  id="nomorHp"
+                  name="nomorHp"
+                  placeholder="08135519***"
+                />
+                <ErrorMessage name="nomorHp" />
+              </Form.Group>
 
-          {formik.touched.nomorHp && formik.errors.nomorHp ? (
-            <div className="error-message">{formik.errors.nomorHp}</div>
-          ) : null}
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Kirimkan
-        </Button>
-      </Form>
+              <Button variant="primary" type="submit">
+                Kirimkan
+              </Button>
+            </Form>
+          )
+        }}
+      </Formik>
     </Container>
   )
 }
---
+++ src/components/oldForm.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Form, Button, Container } from 'react-bootstrap'
+import './form.css'
+
+const initialValues = {
+  nomorHp: ''
+}
+
+const onSubmit = (values) => {
+  console.log('Form data', values)
+}
+
+const validate = (values) => {
+  // values.nomorHp
+  // errors.nomorHp
+  // errors.nomorHp =  "isian ini dibutuhkan"
+
+  let errors = {}
+  if (!values.nomorHp) {
+    errors.nomorHp = 'Required'
+  }
+  return errors
+}
+
+const validationSchema = Yup.object().shape({
+  nomorHp: Yup.string().required('Diperlukan')
+})
+
+const OldIndexPage = () => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema
+    //    validate
+  })
+
+  return (
+    <Container>
+      {console.log('Visited field', formik.errors)}
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group>
+          <Form.Label>Nomor HP (Opsional)</Form.Label>
+          <Form.Control
+            type="text"
+            id="nomorHp"
+            name="nomorHp"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.nomorHp}
+            placeholder="08135519***"
+            className={
+              formik.touched.nomorHp && formik.errors.nomorHp ? 'error' : null
+            }
+          />
+
+          {formik.touched.nomorHp && formik.errors.nomorHp ? (
+            <div className="error-message">{formik.errors.nomorHp}</div>
+          ) : null}
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Kirimkan
+        </Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default OldIndexPage
